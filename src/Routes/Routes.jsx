7c42a2478a0e96d7153b1c0fd3980d7885bc6fd9
pirates <p@mate.localhost>
2023-06-30 23:00:29 +0530

    router-setup-done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+    createBrowserRouter,
+    
+  } from "react-router-dom";
+  
+
+
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
