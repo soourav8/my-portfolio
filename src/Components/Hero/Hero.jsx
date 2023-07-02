cb45dc1599301c943eb83177c4bd6d0339430d22
pirates <p@mate.localhost>
2023-07-03 03:40:06 +0530

    fancy border radious done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,23 +1,32 @@
 import React from "react";
-import {  Cursor, useTypewriter } from "react-simple-typewriter";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import './Hero.css'
 
 const Hero = () => {
-  const [ text ] = useTypewriter({
-    words: ['Full Stack Developer', 'MERN Stack Developer', 'WordPress Developer', 'Graphic Designer'],
+  const [text] = useTypewriter({
+    words: [
+      "Full Stack Developer",
+      "MERN Stack Developer",
+      "WordPress Developer",
+      "Graphic Designer",
+      "Front End Developer",
+    ],
     loop: {},
-    
   });
   return (
-    <div style={{ height: "90vh" }} className="ml-6 md:ml-8">
+    <div style={{ height: "90vh" }} className="ml-6 md:ml-8 flex justify-between">
       <div className="mt-4 md:mt-[8rem]">
         <p className="text-[#6BC7F1]">Hi, my name is,</p>
         <h1 className="text-3xl md:text-7xl   md:font-normal md:leading-normal">
           Sourav Sardar
         </h1>
         <h2 className="text-2xl md:text-5xl md:font-normal text-[#575555]">
-
-          I am a {text} <span/> <Cursor/>
+          I am a {text} <span /> <Cursor />
         </h2>
+      </div>
+
+      <div className="box mt-4 md:mt-[8rem]">
+        
       </div>
     </div>
   );
